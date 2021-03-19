2eadc98c08c6e1f7ccc571fe478e36b927eeeea3
augusto367 <80965091+augusto367@users.noreply.github.com>
2021-03-19 11:24:12 -0300

    Delete prog02.py

--- prog02.py
+++
@@ -1,9 +0,0 @@
-mensagem = 'Hello World'
-
-print('Mensagem:', mensagem)
-print('Tamanho:', len(mensagem))
-print('Primeira letra:', mensagem[0])
-print('Em maiúsculo:', mensagem.upper())
-print('Substituição:', mensagem.replace('World', 'Universe'))
-print('Adição de strings:', 'Hi, ' + mensagem)
-
