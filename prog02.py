cc33052bef5e0a22024458133133523323b96531
augusto367 <80965091+augusto367@users.noreply.github.com>
2021-03-19 11:23:16 -0300

    Add files via upload

---
+++ prog02.py
@@ -0,0 +1,9 @@
+mensagem = 'Hello World'
+
+print('Mensagem:', mensagem)
+print('Tamanho:', len(mensagem))
+print('Primeira letra:', mensagem[0])
+print('Em maiúsculo:', mensagem.upper())
+print('Substituição:', mensagem.replace('World', 'Universe'))
+print('Adição de strings:', 'Hi, ' + mensagem)
+
