241238a2445e0de50f8b95bf938df27cc19ee4cf
augusto367 <80965091+augusto367@users.noreply.github.com>
2021-04-01 14:56:56 -0300

    Update matplot01.py

--- matplot01.py
+++ matplot01.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-x = [1, 2, 3]
+x = [1, 2, 3]  #Definindo as coordenadas x e y
 y = [2, 4, 6]
 
-plt.plot(x,y, 'b^--', label = '2x')
+plt.plot(x,y, 'b^--', label = '2x')  #Comando para plotar o gráfico
 
-x2 = np.arange(0,4.5,0.4)
-plt.plot(x2, x2**2)
+x2 = np.arange(0,4.5,0.4)     #Definindo a coordenada x2
+plt.plot(x2, x2**2)       #Comando para plotar o segundo gráfico
 
-plt.title('Primeiro gráfico', fontdict = {'fontname':'Comic Sans MS', 'fontsize':20})
-plt.xlabel('Eixo X')
+plt.title('Primeiro gráfico', fontdict = {'fontname':'Comic Sans MS', 'fontsize':20})       #Titulo
+plt.xlabel('Eixo X')    #Legenda dos eixos
 plt.ylabel('Eixo Y')
 
 plt.xticks([0,1,2,3,4,5])
